1021dbf4f1a556c49cf947fb9d8a9105e7b97ea0
GatraSatya <156332330+anezacode@users.noreply.github.com>
2025-02-03 13:30:14 +0700

    Delete multi_user directory

--- multi_user/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$koneksi = mysqli_connect("localhost","root","","multi_user");
-
-if (mysqli_connect_errno()){
-    echo "Koneksi database gagal: ".mysqli_connect_error();
-}
-echo "Koneksi database berhasil. ";
-?>
