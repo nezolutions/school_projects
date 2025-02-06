35af08b63ed36addb45a759c1c5eed4cd885ba39
GatraSatya <156332330+anezacode@users.noreply.github.com>
2025-02-06 15:01:10 +0700

    Add files via upload

---
+++ bmi_calculator/Program.cs
@@ -0,0 +1,35 @@
+﻿// ANGGA XPPLG1
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("[GATRASATYA]");
+        Console.Write("Masukkan Berat Badan (Kg): ");
+        double beratBadan = double.Parse(Console.ReadLine());
+
+        Console.Write("Masukkan Tinggi Badan (Cm): ");
+        double tinggiCm = double.Parse(Console.ReadLine());
+        double tinggiMeter = tinggiCm / 100;
+
+        double bbt = beratBadan / (tinggiMeter * tinggiMeter);
+
+        string kategori = "";
+        if (bbt < 18.5)
+        {
+            kategori = "Kurus";
+        }
+        else if (bbt >= 18.5 && bbt <= 22.9)
+        {
+            kategori = "Normal";
+        }
+        else
+        {
+            kategori = "Obesitas";
+        }
+
+        Console.WriteLine("BMI Anda adalah: " + bbt.ToString("F2"));
+        Console.WriteLine("Kategori BMI Anda: " + kategori);
+    }
+}
---
+++ volume_geometry/Program.cs
@@ -0,0 +1,50 @@
+﻿// ANGGA XPPLG1
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("[GATRASATYA]");
+        Console.Write("Pilih Benda yang Ingin Volumenya di Hitung:\n 1. Kubus\n 2. Balok\n 3. Tabung\n<Ketik 1 / 2 / 3>: ");
+        string volume = Console.ReadLine();
+        
+        if (volume.ToLower() == "1") {
+            Console.WriteLine("---------------");
+            Console.Write("Anda Memilih Kubus.\nMasukkan Sisi Kubus: ");
+            double skubus = Convert.ToDouble(Console.ReadLine());
+            
+            double vkubus = skubus * skubus * skubus;
+            Console.WriteLine("---------------");
+            Console.WriteLine("Volume Kubus: " + vkubus + "\nProgram Selesai.\n\"Run\" Untuk Menjalankan Program Kembali.");
+        }
+        else if (volume.ToLower() == "2") {
+            Console.WriteLine("---------------");
+            Console.Write("Masukkan Panjang: ");
+            double pbalok = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Masukkan Lebar: ");
+            double lbalok = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Masukkan Tinggi: ");
+            double tbalok = Convert.ToDouble(Console.ReadLine());
+            
+            double vbalok = pbalok * lbalok * tbalok;
+            Console.WriteLine("---------------");
+            Console.WriteLine("Volume Balok: " + vbalok + "\nProgram Selesai.\n\"Run\" Untuk Menjalankan Program Kembali.");
+        }
+        else if (volume.ToLower() == "3") {
+            Console.WriteLine("---------------");
+            Console.Write("Masukkan Jari-Jari: ");
+            double rtabung = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Masukkan Tinggi: ");
+            double ttabung = Convert.ToDouble(Console.ReadLine());
+
+            double vtabung = Math.PI * Math.Pow(rtabung, 2) * ttabung;
+            Console.WriteLine("---------------");
+            Console.WriteLine("Volume Tabung: " + vtabung + "\nProgram Selesai.\n\"Run\" Untuk Menjalankan Program Kembali.");
+        }
+        else {
+            Console.WriteLine("---------------");
+            Console.WriteLine("Input Tidak Valid, Program Berakhir.\n\"Run\" Untuk Menjalankan Program Kembali.\nNote: Jika Ini Sebuat Kesalahan, Jalankan Ulang Programnya.");
+        }
+    }
+}
